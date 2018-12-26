d2a0e4ea80402f66dc077ad996fbedc8ec3e3e03
chengshun.zeng <zcs@sxw.cn>
2018-12-26 12:01:26 +0800

    first commit

---
+++ jitpacklib/src/main/java/com/zcs/jitpacklib/ZenUtils.java
@@ -0,0 +1,12 @@
+package com.zcs.jitpacklib;
+
+/**
+ * Created by ZengCS on 2018/12/26.
+ * E-mail:zcs@sxw.cn
+ * Add:成都市天府软件园E3-3F
+ */
+public class ZenUtils {
+    public static String getMsg() {
+        return "Hello ZengCS";
+    }
+}
